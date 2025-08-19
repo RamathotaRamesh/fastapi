7125800000ef421df528332888b49d1bfdfb9cfc
Ramesh Ramathota <133636899+RamathotaRamesh@users.noreply.github.com>
2025-08-19 10:29:07 +0530

    Put and Delete api's

--- app/routes/users.py
+++ app/routes/users.py
@@ -32,7 +32,7 @@ async def get_user(user_id: int):
 
 @router.post("/add", response_model=UserResponse)
 async def create_user(user: UserCreate):
-    user_id = len(users_db) + 1
+    user_id = 1 if not users_db else users_db[-1]["id"] + 1
     new_user = {"id": user_id, **user.model_dump()}
     users_db.append(new_user)
     return JSONResponse(
@@ -40,4 +40,25 @@ async def create_user(user: UserCreate):
        content={"message": f"User {user_id} created successfully","data":new_user}
     )
 
+@router.put("/{user_id}", response_model=UserResponse)
+async def update_user(user_id: int, user: UserCreate):
+    for index, existing_user in enumerate(users_db):
+        if existing_user["id"] == user_id:
+            updated_user = {"id": user_id, **user.model_dump()}
+            users_db[index] = updated_user
+            return JSONResponse(
+                status_code=200,
+                content={"message": f"User {user_id} updated successfully", "data": updated_user}
+            )
 
+
+@router.delete("/{user_id}")
+async def delete_user(user_id: int):
+    global users_db
+    for index, user in enumerate(users_db):
+        if user["id"] == user_id:
+            del users_db[index]
+            return JSONResponse(
+                status_code=200,
+                content={"message": f"User {user_id} deleted successfully"}
+            )
